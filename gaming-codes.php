b459d6de1979a3c8d01c5b0c9704fd9343c31c84
Detective <Detective@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-04 17:18:56 +0000

    Uploading v1.1.
    git-svn-id: https://plugins.svn.wordpress.org/gaming-codes/gaming-codes@44246 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gaming-codes.php
+++ gaming-codes.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Gaming Codes
-Plugin URI: http://www.ryuuko.cl/desarrollo/gaming-codes
+Plugin URI: http://www.ryuuko.cl/desarrollo
 Description: A plugin that allows your users to have Gamer Codes. Also, if you have 'El Aleph' (another plugin) installed, you can make list of users for each console.
-Version: 1.0
+Version: 1.1
 Author URI: http://alumnos.dcc.uchile.cl/~egraells
 
 This plugin is licensed under the terms of the General Public License. Please see the file license.txt.
@@ -16,7 +16,6 @@ add_action('init', 'gaming_codes_initialize', 100);
 
 function gaming_codes_initialize() {
 	global $gaming_fields;
-	if (!class_exists('ElAleph')) return;
 	
 	$gaming_fields = array(
 		'nds' => array('id' => 'gaming_code-nds', 'type' => __('console', 'gaming-codes'), 'description' => __("Nintendo DS Friend Code", "gaming-codes")),
@@ -26,6 +25,7 @@ function gaming_codes_initialize() {
 		'ggpo' => array('id' => 'gaming_code-ggpo', 'type' => __('emulation', 'gaming-codes'), 'description' => __("GGPO.net Username", "gaming-codes"))
 	);	
 	
+	load_plugin_textdomain("gaming-codes", 'wp-content/plugins/gaming-codes');	
 }
 
 register_activation_hook(__FILE__, 'gaming_codes_activation');
@@ -47,9 +47,9 @@ function gaming_codes_form() {
 	$user_id = $userdata->ID;
 
 	if (isset($_GET['user_id'])) $user_id = (int) $_GET['user_id'];
-	if ($user_id < 0) wp_die(__("What are you looking at?", "aleph"));
+	if ($user_id < 0) wp_die(__("What are you looking at?", "gaming-codes"));
 
-	echo '<h3>' . __('Gaming Codes', 'aleph') . '</h3>';
+	echo '<h3>' . __('Gaming Codes', 'gaming-codes') . '</h3>';
 	echo '<table class="form-table"><tbody>';
 
 	foreach ($gaming_fields as $field) {
@@ -73,6 +73,8 @@ function gaming_codes_process_form($user_id) {
 	foreach ($gaming_fields as $slug => $field) {
 		if (isset($_POST[$field['id']]))
 			update_usermeta($user_id, $field['id'], stripslashes($_POST[$field['id']]));
+		else
+			delete_usermeta($user_id, $field['id']);
 	}
 }
 
@@ -143,28 +145,16 @@ add_filter('users_where', 'gaming_code_users_where');
 function gaming_code_users_where($where) {
 	global $wpdb, $el_aleph_uq, $gaming_fields;
 	if ('' != get_query_var('gaming_code')) {
-		if (isset($gaming_fields[get_query_var('gaming_code')])) {
-			$el_aleph_uq->queried_object = new stdClass;
-			/// @todo: have a better queried object
-			$el_aleph_uq->queried_object = $gaming_fields[get_query_var('gaming_code')];
+		$slug = get_query_var('gaming_code');
+		if (isset($gaming_fields[$slug])) {
+			$el_aleph_uq->queried_object = $gaming_fields[$slug];
 			$meta_key = preg_replace('|[^a-z0-9_]|i', '', $el_aleph_uq->queried_object['id']); // from update usermeta
 			$where .= " AND $wpdb->usermeta.meta_key = '$meta_key' ";
 		} 
-		else $where .= " AND 0 ";
+		else 
+			$where .= " AND 0 ";
 	}
 	return $where;
-}
-
-/// filter for fake post title
-
-add_filter('users_template_title', 'gaming_code_page_title');
-
-function gaming_code_page_title($title) {
-	global $gaming_fields;
-	if (isset($gaming_fields[get_query_var('gaming_code')])) 
-		return sprintf(__("Users with a %s", 'gaming-codes'), $gaming_fields[get_query_var('gaming_code')]['description']);
-	else 
-		return $title;
 }
 
 /// conditional tag for your templates
@@ -188,6 +178,48 @@ function gaming_codes_rewrite_rules($rewrite) {
 	}
 
 	return $gaming_rules + $rewrite;
+}
+
+/// user lists links
+
+add_filter('aleph_user_lists', 'gaming_codes_user_lists');
+
+function gaming_codes_user_lists($lists) {
+	global $wp_rewrite, $gaming_fields;
+	if ($wp_rewrite->using_permalinks()) {
+		foreach ($gaming_fields as $name => $label)
+			$lists[] = '<a href="' . get_option('siteurl') . '/' . __('searching', 'aleph') . '/' . __('people', 'aleph') . '/' . $label['type'] . '/' . $name . '">' . sprintf(__("With a %s", 'gaming-codes'), $label['description']) . '</a>';
+	} else {
+		foreach ($gaming_fields as $name => $label) 
+			$lists[] = '<a href="' . get_option('siteurl') . '?pagename=author-interests&gaming_code=' . $name . '">' . sprintf(__("With a %s", 'gaming-codes'), $label['description']) . '</a>';	
+	}
+	return $lists;
+}
+
+/// user lists template title
+
+add_filter('aleph_user_template_title', 'gaming_codes_aleph_template_title');
+
+function gaming_codes_aleph_template_title($title) {
+	global $gaming_fields;	
+	if (is_gaming_code_listing()) {
+    	if (have_users())
+    		$title = sprintf(__("Users with a %s", 'gaming-codes'), $gaming_fields[get_query_var('gaming_code')]['description']);
+        else 
+            $title = __("Sorry", "aleph");
+	}
+	return $title;
+}
+/// filter for fake post title
+
+add_filter('users_template_title', 'gaming_code_page_title');
+
+function gaming_code_page_title($title) {
+	global $gaming_fields;
+	if (isset($gaming_fields[get_query_var('gaming_code')])) 
+		return sprintf(__("Users with a %s", 'gaming-codes'), $gaming_fields[get_query_var('gaming_code')]['description']);
+	else 
+		return $title;
 }
 
 /// template tag to display links to the user lists
--- readme.txt
+++ readme.txt
@@ -12,7 +12,7 @@ Gaming Codes allows each user to enter his gamer codes for "next-gen" consoles.
 
 Gaming Codes allows each user to enter his gamer codes for "next-gen" consoles. These codes can be displayed in posts as author information.
 
-Also, this plugin can use another plugin, "El Aleph", to generate lists of users that have codes for a certain platform.
+Also, this plugin can use another plugin, [El Aleph](http://wordpress.org/extend/plugins/el-aleph "El Aleph on WP Extend"), to generate lists of users that have codes for a certain platform.
 
 You can see an example here: http://www.ryuuko.cl/gente/mrhadoken/ . This is a profile page in El Aleph, showing two gamer codes and the user description.
 
@@ -34,15 +34,26 @@ If you have 'El Aleph' installed:
 
 == Changelog ==
 
+= Version 1.1 =
+
+* Improved installation notes. Added a Screenshot. 
+* Added localization for es_ES.
+* The plugin didn't work if El Aleph wasn't installed. Now this is fixed.
+* Some strings were being translated in another domain, not `gaming-codes`.
+* Now if the user doesn't enter a gaming code, or deletes a previous one, the meta key/value is deleted from DB.
+
 = Version 1.0 =
 
 * Initial Release
 
 == Installation ==
 
-1. Put the file gaming-codes.php in your plugins directory.
-2. Activate the plugin. 
+1. Uncompress the plugin in the subdirectory `gaming-codes` of your plugins directory.
+2. Activate the plugin.
+3. Add the template tag `the_author_gaming_codes` in your post/author templates.
+4. For El Aleph users: Add the template tag `the_user_gaming_codes` in your profile/users templates.
+5. Now your registered users can edit their gaming codes in the Dashboard Profile Edit page.   
 
 == Screenshots ==
 
-None.
+1. An example profile. You can see the user description, some registration details, and two gaming codes.
